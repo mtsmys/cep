e71db1fa4591f6addfa1879f972fd8d8564549df
mtsmys <mtsmys@gmail.com>
2019-04-07 10:14:03 +0900

    [update]Supplement license information of TinyMT

--- src/tinymt/TinyMT32.c
+++ src/tinymt/TinyMT32.c
@@ -6,12 +6,37 @@
  * @author Mutsuo Saito (Hiroshima University)
  * @author Makoto Matsumoto (The University of Tokyo)
  *
- * Copyright (C) 2011 Mutsuo Saito, Makoto Matsumoto,
+ * Copyright (c) 2011, 2013 Mutsuo Saito, Makoto Matsumoto,
  * Hiroshima University and The University of Tokyo.
  * All rights reserved.
- *
- * The 3-clause BSD License is applied to this software, see
- * LICENSE.txt
+ * 
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *     * Neither the name of the Hiroshima University nor the names of
+ *       its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written
+ *       permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  ******************************************************************************/
 
 #include "tinymt/TinyMT32.h"
@@ -74,11 +99,14 @@ static void this_period_certification (TinyMT32 *self)
  * This function initializes the internal state array with a 32-bit
  * unsigned integer seed.
  * 
- * @param self		tinymt state vector.
- * @param seed		a 32-bit unsigned integer used as a seed.
+ * @param[in] self	tinymt state vector.
+ * @param[in] seed	a 32-bit unsigned integer used as a seed.
  */
 void TinyMT32_init (TinyMT32 *self, const uint32_t seed)
 	{
+	//========== Variable ==========
+	int i = 0;
+
 	//===== Check argument =====
 	if (self!=NULL && seed>0)
 		{
@@ -90,14 +118,14 @@ void TinyMT32_init (TinyMT32 *self, const uint32_t seed)
 		self->status[2] = self->mat2;
 		self->status[3] = self->tmat;
 		//=====  =====
-		for (int i = 1; i < TINYMT32_MIN_LOOP; i++)
+		for (i=1; i<TINYMT32_MIN_LOOP; i++)
 			{
 			self->status[i & 3] ^= i + UINT32_C(1812433253) * (self->status[(i - 1) & 3] ^ (self->status[(i - 1) & 3] >> 30));
 			}
 		//=====  =====
 		this_period_certification(self);
 		//=====  =====
-		for (int i = 0; i < TINYMT32_PRE_LOOP; i++)
+		for (i=0; i<TINYMT32_PRE_LOOP; i++)
 			{
 			TinyMT32_next_state(self);
 			}
@@ -117,19 +145,21 @@ void TinyMT32_init (TinyMT32 *self, const uint32_t seed)
  * This function initializes the internal state array,
  * with an array of 32-bit unsigned integers used as seeds
  * 
- * @param self			tinymt state vector.
- * @param init_key		the array of 32-bit integers, used as a seed.
- * @param key_length	the length of init_key.
+ * @param[in] self			tinymt state vector.
+ * @param[in] init_key		the array of 32-bit integers, used as a seed.
+ * @param[in] key_length	the length of init_key.
  */
 void TinyMT32_init_by_array (TinyMT32 *self, uint32_t init_key[], int key_length)
 	{
-	const int lag = 1;
-	const int mid = 1;
-	const int size = 4;
-	int i, j;
-	int count;
-	uint32_t r;
+	//========== Variable ==========
+	int i = 0;
+	int j = 0;
+	int count = 0;
+	uint32_t r = 0;
 	uint32_t * st = &self->status[0];
+	const int LAG = 1;
+	const int MID = 1;
+	const int SIZE = 4;
 
 	st[0] = 0;
 	st[1] = self->mat1;
@@ -143,38 +173,38 @@ void TinyMT32_init_by_array (TinyMT32 *self, uint32_t init_key[], int key_length
 		{
 		count = TINYMT32_MIN_LOOP;
 		}
-	r = this_ini_func1(st[0] ^ st[mid % size] ^ st[(size - 1) % size]);
-	st[mid % size] += r;
+	r = this_ini_func1(st[0] ^ st[MID % SIZE] ^ st[(SIZE - 1) % SIZE]);
+	st[MID % SIZE] += r;
 	r += key_length;
-	st[(mid + lag) % size] += r;
+	st[(MID + LAG) % SIZE] += r;
 	st[0] = r;
 	count--;
 	for (i = 1, j = 0; (j < count) && (j < key_length); j++)
 		{
-		r = this_ini_func1(st[i % size] ^ st[(i + mid) % size] ^ st[(i + size - 1) % size]);
-		st[(i + mid) % size] += r;
+		r = this_ini_func1(st[i % SIZE] ^ st[(i + MID) % SIZE] ^ st[(i + SIZE - 1) % SIZE]);
+		st[(i + MID) % SIZE] += r;
 		r += init_key[j] + i;
-		st[(i + mid + lag) % size] += r;
-		st[i % size] = r;
-		i = (i + 1) % size;
+		st[(i + MID + LAG) % SIZE] += r;
+		st[i % SIZE] = r;
+		i = (i + 1) % SIZE;
 		}
 	for (; j < count; j++)
 		{
-		r = this_ini_func1(st[i % size] ^ st[(i + mid) % size] ^ st[(i + size - 1) % size]);
-		st[(i + mid) % size] += r;
+		r = this_ini_func1(st[i % SIZE] ^ st[(i + MID) % SIZE] ^ st[(i + SIZE - 1) % SIZE]);
+		st[(i + MID) % SIZE] += r;
 		r += i;
-		st[(i + mid + lag) % size] += r;
-		st[i % size] = r;
-		i = (i + 1) % size;
+		st[(i + MID + LAG) % SIZE] += r;
+		st[i % SIZE] = r;
+		i = (i + 1) % SIZE;
 		}
-	for (j = 0; j < size; j++)
+	for (j = 0; j < SIZE; j++)
 		{
-		r = this_ini_func2(st[i % size] + st[(i + mid) % size] + st[(i + size - 1) % size]);
-		st[(i + mid) % size] ^= r;
+		r = this_ini_func2(st[i % SIZE] + st[(i + MID) % SIZE] + st[(i + SIZE - 1) % SIZE]);
+		st[(i + MID) % SIZE] ^= r;
 		r -= i;
-		st[(i + mid + lag) % size] ^= r;
-		st[i % size] = r;
-		i = (i + 1) % size;
+		st[(i + MID + LAG) % SIZE] ^= r;
+		st[i % SIZE] = r;
+		i = (i + 1) % SIZE;
 		}
 	this_period_certification(self);
 	for (i = 0; i < TINYMT32_PRE_LOOP; i++)
